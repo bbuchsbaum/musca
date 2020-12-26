70e0b34b70b59d31ae3fce54fb8f89d6850f8387
Bradley Buchsbaum <bbuchsbaum@Umbrellabird.local>
2020-12-26 11:46:34 -0500

    docs

--- R/bada.R
+++ R/bada.R
@@ -19,6 +19,8 @@ between_class_scatter <- function(X, Y, mu) {
   Reduce("+", res)
   
 }
+
+
 pooled_scatter <- function(X, Y) {
   ina <- as.integer(droplevels(Y))
   s <- crossprod(X)
@@ -75,7 +77,7 @@ bada <- function(y, subject, data, ncomp=2, preproc=center(), resdim=20, rescomp
   label_set <- levels(labels)
   
   subjects <- factor(data$design %>% select(!!subject) %>% pull(!!subject))
-  subset_set <- levels(subjects)
+  subject_set <- levels(subjects)
   
   condition_means <- data %>% multivarious:::summarize_by.multidesign(sfun=colMeans, rlang::quos(y))
   
@@ -88,10 +90,15 @@ bada <- function(y, subject, data, ncomp=2, preproc=center(), resdim=20, rescomp
   ## data split by subject
   sdat <- split(data, subject)
   
+  
+  #global_preproc <- fresh(preproc)(center=Xg)
+  
   ## pre-processors
   proclist <- lapply(seq_along(sdat), function(sd) {
     multivarious:::fresh(preproc) %>% prep()
   })
+  
+  names(proclist) <- as.character(subject_set)
   
   ## subject-split and preprocessed data
   strata <- seq_along(sdat) %>% purrr::map(function(i) {
@@ -100,6 +107,17 @@ bada <- function(y, subject, data, ncomp=2, preproc=center(), resdim=20, rescomp
     Xout <- init_transform(p, Xi)
     multidesign(Xout, sdat[[i]]$design)
   })
+  
+  block_indices <- list()
+  ind <- 1
+  for (i in 1:length(strata)) {
+    block_indices[[i]] <- seq(ind, ind+ncol(strata[[i]]$x)-1)
+    ind <- ind + ncol(strata[[i]]$x)
+  }
+  
+  names(block_indices) <- as.character(subject_set)
+  
+  #block_indices <- lapply()
   
   ## group designs
   Dc <- strata %>% purrr::map(function(s) {
@@ -120,18 +138,87 @@ bada <- function(y, subject, data, ncomp=2, preproc=center(), resdim=20, rescomp
     s$x <- s$x - Xc[levs,,drop=FALSE]
     s
   })
-  #browser()
-  Xall <- do.call(rbind, residual_strata %>% purrr::map( ~ .x$x))
   
-  pca_resid <- pca(Xall, ncomp=residcomp)
+
+  Xresid <- do.call(rbind, residual_strata %>% purrr::map( ~ .x$x))
+  
+  pca_resid <- pca(Xresid, ncomp=resdim, method="irlba")
   Xpca_resid <- scores(pca_resid)
   
   Sw <- within_class_scatter(Xpca_resid, interaction(subjects, labels))
-  Sb <- between_class_scatter(Xpca_resid, interaction(subjects, labels), colMeans(Xpca_resid))
+  Sb <- between_class_scatter(Xpca_resid, interaction(subjects, labels), 
+                              colMeans(Xpca_resid))
   
-  eigout <- RSpectra::eig(solve(Sw, Sb))
-  scores <- Xpca_resid %*% eigout$vectors
-  browser()
+  eigout <- eigen(solve(Sw, Sb))
+  #scores <- Xpca_resid %*% eigout$vectors
+  resid_v <- pca_resid$v %*% eigout$vectors
+  v <- cbind(pca_group$v, resid_v)
+  vq <- qr(v)
+  v <- qr.Q(vq)
+  
+  ## compute projections, one subject at a time.
+  s <- do.call(rbind, strata %>% purrr::map(function(s) {
+    s$x %*% v
+  }))
+  
+  #s <- Xall %*% v
+
+  proc <- multivarious:::concat_pre_processors(proclist, block_indices)
+  
+  multivarious:::discriminant_projector(v=v, s=s, apply(s,2,sd), 
+                                        preproc = proc,
+                                        proclist = proclist,
+                                        labels=labels, 
+                                        subjects=subjects,
+                                        barycenters=Xc,
+                                        block_indices=block_indices,
+                                        classes="bada")
  
+}
+
+
+#' @export
+reprocess.bada <- function(x, new_data, colind=NULL, subject=NULL) {
+  if (is.null(colind) && is.null(subject)) {
+    chk::chk_equal(ncol(new_data), shape(x)[1])
+    ## pre-process every which way...
+    Reduce("+", lapply(seq_along(x$block_indices), function(i) {
+      apply_transform(x$preproc, new_data, colind=x$block_indices[[i]])
+    }))/length(x$block_indices)
+    
+  } else if (!is.null(subject)) {
+    ## pre-process along one block
+    chk::chk_character(subject)
+    chk::chk_equal(ncol(new_data), shape(x)[1])
+    sind <- x$block_indices[[subject]]
+    if (!is.null(colind)) {
+      ## relative subset using colind
+      cind <- sind[colind]
+    }
+    apply_transform(x$preproc, new_data, colind=cind)
+  } else {
+    ## colind not null. pre-process every which way using colind per block
+    Reduce("+", lapply(seq_along(x$block_indices), function(i) {
+      apply_transform(x$preproc, new_data, colind=colind)
+    }))/length(x$block_indices)
+  }
+  
+}
+
+#' project 
+#' 
+#' @param subject the `character` id for the subject to project on. 
+#' This is required for subject-specific pre-processing. 
+#' If missing, the group average pre-processing parameters are used.
+#' 
+#' @export
+#' @rdname project
+project.bada <- function(x, new_data,subject) {
+  if (missing(subject)) {
+    NextMethod(x,new_data)
+  } else {
+    #Xp <- multivarious::apply_transform(preproc, new_data)
+    reprocess(x, new_data, subject=subject) %*% coef(x)
+  }
 }
 
